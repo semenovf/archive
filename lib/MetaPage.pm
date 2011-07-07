80f3c068e57e166708f19dee8111dfd51c581da8
Fedor Semenov <fedor.v.semenov@gmail.com>
2011-07-07 16:47:35 +0600

    modified:   lib/MetaPage.pm modified:   lib/MetaPage/HandlerFactory.pm

--- lib/MetaPage.pm
+++ lib/MetaPage.pm
@@ -1,205 +1,86 @@
+#
+# Copyright (C) 2010,2011 Vladislav Trifochkin
+# MetaPage is a part of Met@Page software
+# Met@Page is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+
 package MetaPage;
-use MetaPage::HandlerFactory;
-use MetaPage::Parser;
-use MetaPage::Exception;
+use Carp;
+use base qw(XML::Parser::Expat);
+
 use strict;
 use warnings;
 
-use base qw(JQ::App);
+sub PREFIX {'.prefix'}
+sub TEXT   {'.text'}
 
-use constant TEMPLATES_DIR => 'tpl';
-
-__PACKAGE__->mk_accessors(qw(parser tmplMainPath tmplAppPath tmplPrefsPath));
-
-
-sub new
+sub parse_text
 {
-    my $class = shift or die;
-    my $self = $class->SUPER::new(@_);
-    logging_debug( $self->pref('verbose') );
-    logging_encoding( $self->pref('logger_encoding') ) if defined $self->pref('logger_encoding');
-    default_encoding( $self->pref('encoding') ) if defined $self->pref('encoding');
-    $self->parser( MetaPage::Parser->new() );
-    MetaPage::HandlerFactory->handlersFor( $self->parser );
-
-    $self->tmplMainPath (
-        $self->isWebApp
-            ? File::Spec->catfile(TEMPLATES_DIR, 'index.pl.tpl')
-            : File::Spec->catfile(TEMPLATES_DIR, 'main.pl.tpl')
-    );
-
-    $self->tmplAppPath (
-        $self->isWebApp
-            ? File::Spec->catfile(TEMPLATES_DIR, 'WebApp.pm.tpl')
-            : File::Spec->catfile(TEMPLATES_DIR, 'App.pm.tpl')
-    );
-
-    $self->tmplPrefsPath( File::Spec->catfile(TEMPLATES_DIR, 'Prefs.pm.tpl') );
-
-    return bless $self, $class;
-}
-
-sub isWebApp
-{
-    my $self = shift or die;
-    return $self->pref('webapp') ? 1 : 0;
-}
-
-sub exec
-{
-    my $self = shift or die;
-    my $parser = $self->parser;
-
-    SWITCH: {
-        $self->pref('project') && do {
-            $self->genProjectTemplate();
-            last SWITCH;
-        };
-
-        $self->pref('create') && do {
-            JQ::Exception::InvalidValue->throw('project must be specified') unless $self->pref('project');
-
-            eval {
-                $self->makeProjectTree();
-                $self->genSamples();
-                    #$self->genSampleApp();
-                    #$self->genSamplePrefs();
-                    #foreach my $file (@ARGV) {
-                    #    $parser->parsefile( $file, ErrorContext=>3 );
-                    #    $parser->renderCode();
-                    #}
-            }; if( $@ ) {
-                die $@;
-            #       $self->purgeProjectTree();
-            }
-            last SWITCH;
-        };
-    }
-}
-
-sub makeProjectTree
-{
-    my $self = shift or die;
-
-    my $project_dir = File::Spec->catfile($self->pref('base-dir'), $self->pref('project'));
-    JQ::Exception::IO->throw( sprintf('%s: project directory already exists', $project_dir) )
-        if -e $project_dir;
-
-    make_path( $project_dir, { verbose=>0, error=>\my $errors} );
-    if( @$errors ) {
-        my $plain_err_text = 'make path error(s) occured:';
-        foreach( @$errors ) {
-            $plain_err_text .= '\n\t';
-            my ( $f, $e ) = %$_;
-            if( $f eq '' ) {
-                $plain_err_text .= $e;
-            } else {
-                $plain_err_text .= $f.': '.$e;
-            }
-        }
-        JQ::Exception::IO->throw( sprintf('%s: %s', $project_dir, $plain_err_text) );
-    }
-
-    return 1;
+    shift->SUPER::parse(join '', @_);
 }
 
 
-sub purgeProjectTree
+sub parse_file
 {
-    my $self = shift or die;
-#    my $path = shift or die ExArgumentExpected;
-    #-e $path and die ExPathExists;
-}
-
-# usage:
-# $self->_doCommonReplacement( $string_ref )
-#
-sub _doCommonReplacement
-{
-    my $self = shift or die;
-    my $text = shift or die;
-    #my ${_package_name_}    = $self->pref('project') or JQ::Exception::InvalidValue->throw( 'project' );
-    #my ${_program_name_}    = $self->pref('program') or JQ::Exception::InvalidValue->throw( 'program' );
-    #my ${_program_version_} = $self->pref('version');
-    #my ${_program_author_}  = $self->pref('author');
-
-    my (
-        ${_package_name_},
-        ${_program_name_},
-        ${_program_version_},
-        ${_program_author_} ) = map { $self->pref($_) or JQ::Exception::InvalidValue->throw( sprintf("invalid value for preference '%s'", $_) ); }
-            qw(project program version author);
-
-    if( ref $text ) {
-        die unless ref $text eq 'SCALAR';
-        return eval "\"$$text\"";
-    }
-    return eval "\"$text\"";
-}
-
-# Generates samples from templates
-#
-sub genSamples
-{
-    my $self = shift or die;
+    my ($self, $path) = @_;
+    
+    open my $fh, '>', $path or croak sprintf('%s: %s', $path, $!);
     local $/;
-
-    foreach ((
-        $self->tmplMainPath,
-        $self->tmplAppPath,
-        $self->tmplPrefsPath
-    )) {
-        open my $fh, '<', $_
-            or JQ::Exception::IO->throw( sprintf('%s: %s', $_, $!) );
-        my $text = <$fh>;
-        close $fh;
-        print $self->_doCommonReplacement(\$text);
-    }
+    $self->SUPER::parse(<$fh>);
+    close $fh;
 }
 
 
-sub genProjectTemplate
+sub _append # [protected]
 {
-    my $self = shift or die;
-    my $fh = shift || *STDOUT;
+    my ($self, $what, @data) = @_;
+    $self->{$what} = [], unless defined $self->{$what};
+    push @{$self->{$what}}, @data;
+}
 
-    my (
-        $_perlbin_,
-        $_basedir_,
-        $_projectname_,
-        $_bootstrap_,
-        $_version_,
-        $_author_,
-        $_apptype_
-    ) = map { (my $v = $_) =~ s/\\/\\\\/g; $v; } (
-        JQ::Utils::Perl::perl_interpreter,
-        'need_to_define_basedir_for_project',
-        'project_name',
-        'bootstrap_name.pl',
-        '0.0.1',
-        getlogin() || getpwuid($<) || 'your_author_name',
-        'consoleapp'
-    );
+sub render
+{
+    my $self = $_[0];
+    foreach( @{$self->{&PREFIX}} ) {
+        print $_; 
+    }
 
-    $_apptype_ = 'webapp' if $self->prefs('webapp');
+    foreach( @{$self->{&TEXT}} ) {
+        print $_; 
+    }
 
-    my $xml = <<EOXML;
-<?xml version=\\"1.0\\"?>
-<metapage>
-    <${_apptype_}>
-        <perlbin>${_perlbin_}</perlbin>
-        <basedir>${_basedir_}<basedir>
-        <project>${_projectname_}</project>
-        <bootstrap>${_bootstrap_}</bootstrap>
-        <version>${_version_}</version>
-        <author>${_author_}</author>
-    </${_apptype_}>
-</metapage>
-EOXML
-
-    $xml = eval "\"$xml\"";
-
-    print $fh $xml;
 }
 
 1;
+
+__END__
+
+=head1
+
+use MetaPage;
+use MetaPage::HandlerFactory;
+
+my $mp = MetaPage->new;
+MetaPage::HandlerFactory->handlersFor($mp);
+
+or
+
+my $mp = MetaPage::HandlerFactory->handlersFor(MetaPage->new);
+
+...
+
+local $/;
+open my $fh, '<', '/path/to/file' or die;
+$mp->parse_text(<$fh>);
+close $fh;
+
+or
+
+$mp->parse_file('/path/to/file');
+
+...
+
+$mp->render;
+
+=cut
--- lib/MetaPage/HandlerFactory.pm
+++ lib/MetaPage/HandlerFactory.pm
@@ -1,231 +1,96 @@
 #
-# Copyright (C) 2010 Vladislav Trifochkin
+# Copyright (C) 2010,2011 Vladislav Trifochkin
 # MetaPage::HandlerFactory is a part of Met@Page software
 # Met@Page is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
 package MetaPage::HandlerFactory;
-use Module::Load;
-use Module::Loaded;
-use Params::Validate qw(:all);
-use JQ::Logger;
-use JQ::Utils::Text::Trim;
-use MetaPage::Exception;
+use Carp;
+use MetaPage;
+#use Text::Simplify;
+
 use strict;
+use warnings;
 
-use constant ROOT_ELEM => 'metapage';
-
-=head
-    MetaPage::HandlerFactory->handlersFor( $xml_parser )
+=head1
+    
+    MetaPage::HandlerFactory->handlersFor( $parser )
+    
 =cut
-sub handlersFor {
-    my ($class, $xml_parser) = validate_pos( @_, 1,
-        {
-            type=>OBJECT,
-            callbacks=>{
-                'MetaPage::Parser object' => sub { ref $_[0] && ref $_[0] eq 'MetaPage::Parser' }
-            }
-        }
-    );
-    $xml_parser->setHandlers (
-        Start        => \&_onStartElement,
-        End          => \&_onEndElement,
-        Char         => \&_onText,
-        Proc         => \&_onProc,
-        Comment      => \&_onComment,
-        CdataStart   => \&_onCDataStart,
-        CdataEnd     => \&_onCDataEnd,
-        Default      => \&_onDefault,
-        Unparsed     => \&_onUnparsed,
-        Notation     => \&_onNotation,
-        ExternEnt    => \&_onExternEnt,
-        ExternEntFin => \&_onExternEntFin,
-        Entity       => \&_onEntity,
-        Element      => \&_onElement,
-        Attlist      => \&_onAttlist,
-        Doctype      => \&_onDoctype,
-        DoctypeFin   => \&_onDoctypeFin,
-        XMLDecl      => \&_onXmlDecl
-    );
-    $xml_parser->indent(0);
-    $xml_parser->increment(4);
 
-    my $elements = $xml_parser->elements;
+my %_handlers = (
+    Start        => \&_on_start_elem,
+    End          => \&_on_end_elem,
+    Char         => \&_on_text,
+    Comment      => \&_on_comment
+);
+
+my %_mp_handlers = (
+    'use' => \&_on_mp_use
+);
+
+
+sub handlersFor {
+    my ($class, $parser) = @_;
+    $parser->setHandlers ( %_handlers );
+    return $parser;
 }
 
-sub _onStartElement # (Expat, Element [, Attr, Val [,...]])
+sub _on_start_elem # (Expat, Element [, Attr, Val [,...]])
 {
-    my $xml_parser = shift or die;
-    my $elem = shift or die;
-    my %atts = @_;
+    my ($parser, $elem, %atts) = @_;
 
-    my $elements = $xml_parser->elements;
-
-    if( %{$elements} ) {
-        MetaPage::Exception::ParseError->throw("unknown element: $elem" )
-            unless exists $elements->{$elem};
-
-        SWITCH: {
-            $elem eq 'use' and do {
-                SWITCH_0: {
-                    MetaPage::Exception::XMLAttributeExpected->throw( "attribite 'as' expected for element '$elem'") unless exists $atts{'as'};
-                    exists $atts{'webject'} and do {
-                        $xml_parser->addCode( sprintf("use %s;", $atts{'webject'}) );
-                        $elements->{$atts{'as'}} = $atts{'webject'};
-                        last SWITCH_0;
-                    };
-                    exists $atts{'layout'} and do {
-                        $elements->{$atts{'as'}} = $atts{'layout'};
-                        $xml_parser->addCode( sprintf("use %s;", $atts{'layout'}) );
-                        last SWITCH_0;
-                    };
-                    MetaPage::Exception::XMLUnexpectedAttribute->throw("unexpected attribute for '$elem'");
-                }
-                last SWITCH;
-            };
-
-            exists $elements->{$elem} and do {
-                $xml_parser->addCode( sprintf('%s->new(', $elements->{$elem}) );
-                foreach my $a ( keys %atts ) {
-                    $xml_parser->addCode( sprintf(' %s=>"%s"', $a, $atts{$a} ) );
-                }
-                $xml_parser->addCode(");");
-                last SWITCH;
-            }
-
+    if( $elem =~ /^mp:(.+)$/ ) {
+        my $mpt = $1;
+        
+        if( exists $_mp_handlers{$mpt} ) {
+            $_mp_handlers{$mpt}->($parser, \%atts);
+        } else {
+            
         }
-
-        #my $pkgName = "MetaPage::Parser::$elem";
-        #load $pkgName;
-        #die ExParseError($xml_parser, "unable to load module: $pkgName, check if it exists" )
-        #    unless is_loaded($pkgName);
-        #$pkgName->parse( $xml_parser, $elem, %atts );
-
     } else {
-        MetaPage::Exception::ParseError->throw(
-            sprintf("may be it is not MetaPage resource file: root element '%s' expected", ROOT_ELEM) )
-            unless $elem eq ROOT_ELEM;
-        $elements->{'use'} = 1;
+        $parser->_append(MetaPage::TEXT, "<$elem>"); # TODO: need to add attributes
     }
 }
 
-sub _onEndElement # (Expat, Element)
+sub _on_end_elem # (Expat, Element)
 {
-    my ($xml_parser, $elem) = @_;
+    my ($parser, $elem) = @_;
+    if( $elem =~ /^mp:(.+)$/ ) {
+        my $mpt = $1;
+    } else {
+        $parser->_append(MetaPage::TEXT, "</$elem>");
+    }
 }
 
-sub _onText # (Expat, String)
+sub _on_text # (Expat, String)
 {
-    my $xml_parser = shift or die;
-    my $text  = shift or die;
-
-    $text = sppacktrim($text);
-    print ' 'x$xml_parser->indent, "$text\n" if $text;
+    my ($parser, $text) = @_;
+    #simplify($text);
+    $parser->_append(MetaPage::TEXT, $text);
 }
 
-sub _onProc # (Expat, Target, Data)
+sub _on_comment # (Expat, Data)
 {
-    my ($xml_parser, $target, $data) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'Proc'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
+    my ($parser, $data) = @_;
+    $parser->_append(MetaPage::TEXT, $data);
 }
 
-sub _onComment # (Expat, Data)
-{
-    my $xml_parser = shift or die;
-    my $text  = shift or die;
-}
 
-sub _onCDataStart # (Expat)
+sub _on_mp_use # (parser, HASHREF)
 {
-    my $xml_parser = shift or die;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'CDataStart'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onCDataEnd # (Expat)
-{
-    my $xml_parser = shift or die;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'CDataEnd'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onDefault # (Expat, String)
-{
-    #my $xml_parser = shift;
-    #my $text  = shift;
-}
-
-sub _onUnparsed # (Expat, Entity, Base, Sysid, Pubid, Notation)
-{
-    my ($xml_parser, $entity, $base, $sysid, $pubid, $notation) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'Unparsed'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onNotation # (Expat, Notation, Base, Sysid, Pubid)
-{
-    my ($xml_parser, $notation, $base, $sysid, $pubid) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'Notation'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onExternEnt # (Parser, Base, Sysid, Pubid)
-{
-    my ($xml_parser, $base, $sysid, $pubid ) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'ExternEnt'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onExternEntFin # (Expat)
-{
-    my $xml_parser = shift or die;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'ExternEntFin'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onEntity # (Expat, Name, Val, Sysid, Pubid, Ndata, IsParam)
-{
-    my ($xml_parser, $name, $val, $sysid, $pubid, $ndata, $isparam) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'Entity'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onElement # (Expat, Name, Model)
-{
-    my ($xml_parser, $name, $model) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'Element'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onAttlist # (Expat, Elname, Attname, Type, Default, Fixed)
-{
-    my ($xml_parser, $elname, $attname, $type, $default, $fixed) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'Attlist'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onDoctype # (Expat, Name, Sysid, Pubid, Internal)
-{
-    my ($xml_parser, $name, $sysid, $pubid, $internal) = @_;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'Doctype'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onDoctypeFin # (Parser)
-{
-    my $xml_parser = shift or die;
-    MetaPage::Exception::XMLUnexpectedEvent->throw("unexpected event 'DoctypeFin'")
-        unless $xml_parser->ignoreXMLUnexpectedEvent;
-}
-
-sub _onXmlDecl # ( Expat, Version, Encoding, Standalone )
-{
-    my ($xml_parser, $version, $encoding, $standalone ) = @_;
-    #die ExXMLUnexpectedEvent( sprintf('XmlDecl [version=%s; encoding=%s; standalone=%s]',
-    #    $version // '<unknown>', $encoding // '<unknown>',
-    #    defined($standalone) ? ($standalone ? 'yes' : 'no') : 'no') )
-    #    unless $xml_parser->ignoreXMLUnexpectedEvent;
+    my ($parser, $atts) = @_;
+    my $webject = $atts->{'webject'};
+    my $as = $atts->{'as'};
+    
+    while(1) {
+        last if $webject;
+        last if $as;
+        croak q(incomplete 'use' tag, use like this: <mp:use webject="SomeWebjectClass" as="Alias" />);
+        return;
+    }
+    
+    $parser->_append(MetaPage::PREFIX, "use $webject;\n");
 }
 
 1;
