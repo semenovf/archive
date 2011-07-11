06d4bbf7723ad3ef8b352faf5b7ad1894f23b21f
Fedor Semenov <fedor.v.semenov@gmail.com>
2011-07-11 15:37:14 +0600

    Webejct::Native added

---
+++ lib/Webject/Native.pm
@@ -0,0 +1,24 @@
+package Webject::Native;
+use base 'Webject';
+
+my @rw_accessors = qw(
+    tag
+);
+
+__PACKAGE__->mk_accessors(@rw_accessors);
+
+sub new {
+    my $class = shift;
+    my $self = bless $class->SUPER::new(@_), $class;
+    $self->value('');
+    return $self;
+}
+
+1;
+
+__DATA__
+
+<!--html{
+<%=$self->value %>
+}html-->
+
