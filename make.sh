786edf9856d94dbf25bd6136665d8316895cb362
Fedor Semenov <fedor.v.semenov@gmail.com>
2012-08-31 16:01:14 +0000

    Added make.sh.

---
+++ make.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+usage()
+{
+  echo Usage
+  echo $0 \[all \| clean\]
+}
+
+
+case $1 in
+  clean)
+    make distclean
+    del Makefile
+    qmake -makefile -r -o Makefile
+    ;;
+  all)
+    qmake -makefile -r -o Makefile
+    make all
+    ;;
+  *)
+    usage
+    exit 1
+esac
+
+exit 0
+
+
