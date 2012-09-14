7ab3bb645457a604ad33c8246a5ab85996e4d86f
unknown <fedor.v.semenov@gmail.com>
2012-09-14 17:13:46 +0500

    make.cmd modified.

--- make.cmd
+++ make.cmd
@@ -25,10 +25,13 @@
 :: For more information about delayed expansion type: SET /? from the command line
 :: --------------------------------------------------------------------------------------------
 
-@call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Debug /x32 /win7 > nul 2>&1
+@call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /Debug /win7 2>&1
 if %ERRORLEVEL% EQU 0 goto begin
-call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86 > nul 2>&1
+call "C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat" x86 2>&1
 if %ERRORLEVEL% GTR 0 goto error_SetEnv
+:: nmake
+:: pause Press any key
+:: if errorlevel goto error_nmakeNotFound
 
 :begin
 if "%1" == "clean" goto makeclean
@@ -50,5 +53,10 @@ goto end
 :error_SetEnv
 echo Error: failed to set environment 1>&2
 goto end
+
+:error_nmakeNotFound
+echo Error: nmake not found 1>&2
+goto end
+
 
 :end
