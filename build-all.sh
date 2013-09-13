502518f35e1ab938392f6a9afb0d366ba1f72c4a
Fedor Semenov <fedor.v.semenov@gmail.com>
2013-09-13 16:21:45 +0600

    Clean project directory before pushing.

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DIRS="cwt-core cwt-io cwt-logger cwt-uri cwt-csv cwt-json cwt-serial cwt-fs cwt-app"
+DIRS="cwt-core cwt-io cwt-uri cwt-json cwt-fs cwt-csv cwt-app"
 
 for dir in $DIRS ; do
     cd $dir
--- git-push.sh
+++ git-push.sh
@@ -7,8 +7,13 @@ for dir in $DIRS ; do
 	if [ -d $dir/.git ] ; then
 	    echo Git pushing for $dir ...
 	    cd $dir
+	    if [ -x clean.sh ] ; then
+		echo Cleaning ...
+		./clean.sh
+	    fi
 	    git add -A
 	    git commit
+	    sleep 1
 	    git push
 	    if [ "$?" != "0" ] ; then
 		echo Git pushing failed. >&2
@@ -16,6 +21,7 @@ for dir in $DIRS ; do
 	    fi
 	    cd ..
 	    echo Git pushing for $dir is OK.
+#        sleep 5
 	fi
     fi
 done
