0a6819b371e0160c5b0b894c08e57b07d50f12b8
Fedor Semenov <fedor.v.semenov@gmail.com>
2013-12-02 17:03:04 +0600

    Minimal implementation of list class.

--- include/pfs/list.hpp
+++ include/pfs/list.hpp
@@ -9,20 +9,25 @@
 #define __PFS_LIST_HPP__
 
 #include <list>
+#include <pfs/shared_ptr.hpp>
+#include <pfs/pimpl.hpp>
 
 namespace pfs {
 
 template <typename T>
-class list : private std::list<T>
+class list
 {
-	typedef std::list<T> base_class;
+	typedef std::list<T> impl;
+	PFS_PIMPL_INLINE(list, impl)
 
 public:
-	typedef typename base_class::iterator iterator;
-	typedef typename base_class::const_iterator const_iterator;
+	typedef typename impl::iterator iterator;
+	typedef typename impl::const_iterator const_iterator;
 
 public:
-	list () : base_class() {}
+	list () : _pimpl(new impl()) {}
+
+	size_t size () const { return _pimpl->size(); }
 };
 
 } // pfs
