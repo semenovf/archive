06946d1c34d0dbc1b709326a640126ca877cf8c7
semenovf <fedor.v.semenov@gmail.com>
2014-02-19 16:57:32 +0600

    Fixed unix version of __strerror().

--- src/unix/strerror_unix.cpp
+++ src/unix/strerror_unix.cpp
@@ -16,10 +16,20 @@ pfs::string __strerror (int_t errn)
 {
 #if defined(_POSIX_C_SOURCE) && _POSIX_C_SOURCE >= 200112L
 	static const int __MaxBufLen = 128;
-
 	char buffer[__MaxBufLen];
+
+// XSI-compliant version
+#	if(_POSIX_C_SOURCE >= 200112L || _XOPEN_SOURCE >= 600) && ! _GNU_SOURCE
+
 	strerror_r(errn, buffer, __MaxBufLen);
 	return pfs::string::fromUtf8(buffer);
+
+#	else // GNU-specific version
+
+	return pfs::string::fromUtf8(strerror_r(errn, buffer, __MaxBufLen));
+
+#	endif
+
 #else
 	static mutex __mutex;
 	auto_lock<> locker(&__mutex);
