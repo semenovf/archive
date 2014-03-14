d41e4f04e3c55d6de9df460adae18981ba22c9f1
semenovf <fedor.v.semenov@gmail.com>
2014-03-14 16:49:38 +0600

    Added persist.hpp (experimental).

---
+++ include/cwt/debby/perist.hpp
@@ -0,0 +1,88 @@
+/**
+ * @file perists.hpp
+ * @author wladt
+ * @date Mar 14, 2014
+ */
+
+#ifndef __CWT_DEBBY_PERISTS_HPP__
+#define __CWT_DEBBY_PERISTS_HPP__
+
+#include <iostream>
+
+namespace cwt { namespace debby {
+
+
+template <typename T>
+class storage_type
+{
+	 _typestr;
+public:
+	storage_type() {}
+
+	pfs::string typestr ();
+};
+
+class deployer
+{
+	bool _nullable;
+	int  _autoinc;
+
+public:
+	deployer ()
+		: _nullable(false)
+		, _autoinc(0) {}
+
+    deployer & operator << (void (*__pf)(deployer &))
+    {
+    	__pf(*this);
+    	return *this;
+    }
+
+	template <typename T>
+	deployer & operator << (void (*__pf)(storage_type<T> &))
+	{
+		__pf(*this);
+		return *this;
+    }
+
+	void setNullable () { _nullable = true; }
+	void setAutoinc (int inc = 1) { _autoinc = inc; }
+};
+
+inline deployer & nullable (deployer & d) { d.setNullable(); return d; }
+inline deployer & autoinc  (deployer & d) { d.setAutoinc(1); return d; }
+
+// deployer() << nullable << storage_type<int>()
+
+
+
+class abstract_persistent
+{
+private:
+	abstract_persistent () {}
+
+public:
+	virtual ~abstract_persistent () { std::cout << std::endl; }
+
+	virtual bool create ();
+	virtual bool get ();
+	virtual bool update ();
+	virtual bool remove ();
+
+	virtual bool deploy ();
+	virtual bool destroy ();
+};
+
+
+template <typename Model>
+class persistent : public abstract_persistent
+{
+	Model * _model;
+
+public:
+	persistent (Model * model) : _model (model) {}
+};
+
+}} // cwt
+
+#endif /* __CWT_DEBBY_PERISTS_HPP__ */
