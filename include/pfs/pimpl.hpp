5f8109d2bc08e65f66ad75e3f3a14acbdf9b0607
Fedor Semenov <fedor.v.semenov@gmail.com>
2015-02-09 17:17:16 +0500

    No changes.

--- include/pfs/pimpl.hpp
+++ include/pfs/pimpl.hpp
@@ -362,4 +362,9 @@ Class::Class (const Class::Impl & other)                       \
 // } TODO DEPRECATED
 #endif
 
+// TODO implement for nullable:
+// 	nil.swap(non-nil)
+//	non-nil.swap(nil)
+//	nil.swap(nil)
+
 #endif /* __PFS_PIMPL_HPP__ */
