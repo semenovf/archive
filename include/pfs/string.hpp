7c718734ccaf36131d79a665e7c2ee0074f98cd3
Fedor Semenov <fedor.v.semenov@gmail.com>
2015-11-10 21:42:50 +0500

    Changed pfs/string.hpp

--- include/pfs/string.hpp
+++ include/pfs/string.hpp
@@ -28,8 +28,32 @@ typedef pfs::mbcs_string<uint8_t>  utf8_string;
 typedef pfs::mbcs_string<uint16_t> utf16_string;
 typedef pfs::mbcs_string<uint32_t> utf32_string;
 
+#if PFS_STRING_UTF16
+
+typedef utf16_string string;
+
+#elif PFS_STRING_UTF32
+
+typedef utf32_string string;
+
+#else
+
 typedef utf8_string string;
-//typedef std::string string;
+
+#endif
+
+/**
+ * @brief Cast UTF-8-encoded c-string into pfs::string.
+ *
+ * @param s Source UTF-8-encoded c-string.
+ * @param result Reference to string to store result.
+ * @return Reference to @c result.
+ */
+inline utf8_string & lexical_cast (const char * s, utf8_string & result)
+{
+	result = pfs::string::fromUtf8(s);
+	return result;
+}
 
 } // pfs
 
