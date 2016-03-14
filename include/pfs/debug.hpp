499dccb78af0cda7073cce8409aec864a3163400
Fedor Semenov <fedor.v.semenov@gmail.com>
2016-03-14 17:11:55 +0500

    Added PFS_ERROR and PFS_WARN macros.

---
+++ include/pfs/debug.hpp
@@ -0,0 +1,16 @@
+/*
+ * debug.hpp
+ *
+ *  Created on: Mar 14, 2016
+ *      Author: wladt
+ */
+
+#ifndef __PFS_DEBUG_HPP__
+#define __PFS_DEBUG_HPP__
+
+#include <iostream>
+
+#define PFS_WARN(x) std::cerr << "WARN: " << x << std::endl
+#define PFS_ERROR(x) std::cerr << "ERROR: " << x << std::endl
+
+#endif /* __PFS_DEBUG_HPP__ */
