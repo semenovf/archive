6106d62e0fe5a3440614f6bf2e6f5c554480d48a
Fedor Semenov <fedor.v.semenov@gmail.com>
2016-07-07 14:49:52 +0500

    Minor changes.

--- src/pfs-io/file.cpp
+++ src/pfs-io/file.cpp
@@ -16,9 +16,7 @@ void file_appender::print (pfs::string const & msg)
 	_d.write(msg.c_str(), msg.size(), & ex);
 	_d.write("\n", 1);
 
-	if (ex) {
-		PFS_ERROR("Failed to write log message: " << to_string(ex));
-	}
+    PFS_ERROR2(ex != 0, _u8("Failed to write log message: ").append(to_string(ex)).c_str());
 }
 
 }
