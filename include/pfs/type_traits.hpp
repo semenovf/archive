075dcfcba603b2dddb3265e88e9a87258eac7c41
Fedor Semenov <fedor.v.semenov@gmail.com>
2016-12-27 17:23:07 +0500

    Moved pfs::cli into pfs-mpl project. Added if_else template.

--- include/pfs/type_traits.hpp
+++ include/pfs/type_traits.hpp
@@ -13,4 +13,21 @@
 #include <pfs/cxxversion.hpp>
 #include PFS_CXX_HEADER(type_traits)
 
+namespace pfs {
+
+template <bool, typename T1, typename>
+struct if_else
+{
+    typedef T1 type;
+};
+
+template <typename T1, typename T2>
+struct if_else<false, T1, T2>
+{
+    typedef T2 type;
+};
+
+}
+
+
 #endif /* __PFS_TYPE_TRAITS_HPP__ */
