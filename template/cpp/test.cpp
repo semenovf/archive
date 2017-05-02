239afc277f0dabdd8caba57ff7690312ebdd4da0
Fedor Semenov <fedor.v.semenov@gmail.com>
2017-05-02 18:32:36 +0500

    Modified template/cpp/test.cpp

--- template/cpp/test.cpp
+++ template/cpp/test.cpp
@@ -14,18 +14,18 @@ using std::endl;
 
 int main(int argc, char *argv[])
 {
-	PFS_UNUSED(argc);
-	PFS_UNUSED(argv);
+    PFS_UNUSED(argc);
+    PFS_UNUSED(argv);
 
-	BEGIN_TESTS(2);
+    BEGIN_TESTS(2);
 
-	TEST_OK(true == true);
-	TEST_FAIL(true == true);
+    TEST_OK(true == true);
+    TEST_FAIL(true == true);
 
-	ADD_TESTS(2);
-	TEST_OK2(true == true, "true == true");
-	TEST_FAIL2(true == true, "true == true");
+    ADD_TESTS(2);
+    TEST_OK2(true == true, "true == true");
+    TEST_FAIL2(true == true, "true == true");
 
-	return END_TESTS;
+    return END_TESTS;
 }
 
