82eeb8342b4347d4d675c41dd492f1be2705d9fc
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-19 09:43:28 +0500

    Merged cwt-abnf.

--- merge.sh
+++ merge.sh
@@ -8,7 +8,8 @@
 #REPO_NAME=pfs-dom
 #REPO_NAME=pfs-app
 #REPO_NAME=pfs-debby-sqlite3
-REPO_NAME=pfs-cli
+#REPO_NAME=pfs-cli
+REPO_NAME=cwt-abnf
 
 git remote add ${REPO_NAME} git@github.com:semenovf/${REPO_NAME}.git
 git fetch ${REPO_NAME}
