e013920f17df53fe65a2afdee5cb1deadd630bbb
Fedor Semenov <fedor.v.semenov@gmail.com>
2018-04-19 09:38:14 +0500

    Merged pfs-app.

--- merge.sh
+++ merge.sh
@@ -5,7 +5,8 @@
 #REPO_NAME=pfs-debby
 #REPO_NAME=pfs-griotte
 #REPO_NAME=pfs-types
-REPO_NAME=pfs-dom
+#REPO_NAME=pfs-dom
+REPO_NAME=pfs-app
 
 git remote add ${REPO_NAME} git@github.com:semenovf/${REPO_NAME}.git
 git fetch ${REPO_NAME}
